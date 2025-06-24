f0ce22f54e04cd966d3e58c79e44c04cac119bdb
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-25 05:48:54 +0600

    C : First Program in C

--- 1_C_Tutorial/1_Home/first.c
+++ 1_C_Tutorial/1_Home/first.c
@@ -1,5 +1,7 @@
 /*
 C : First Program in C 
+C is a general-purpose programming language
+C is very powerful; it has been used to develop operating systems, databases, applications, etc.
 */
 
 #include <stdio.h>
