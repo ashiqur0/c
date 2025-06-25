4923b33fb1449b480035f48906d1d3f6b7483790
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-25 06:09:19 +0600

    C : How to compile and run?

---
+++ 1_C_Tutorial/3_Get_Started/Hello.c
@@ -0,0 +1,17 @@
+/*
+C : How to compile and run?
+
+How to compile and run?
+compile: gcc file_name.c 
+run: ./a.exe
+
+short-cut command: ctrl + alt + n (compile and run)
+*/
+
+#include <stdio.h>
+int main() {
+    
+    printf("Hello World!");
+
+    return 0;
+}
