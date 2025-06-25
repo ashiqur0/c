663fbb81a50ea99fd69aceffb594270da605d81e
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-25 06:35:09 +0600

    C : Output

---
+++ 1_C_Tutorial/5_Output/1_output.c
@@ -0,0 +1,15 @@
+/*
+C : Output
+
+printf() function: is used to print values or text.
+print text: keep the text inside "" (double quotes)
+*/
+
+#include <stdio.h>
+int main() {
+    
+    printf("Hello World!");
+    printf("Have a good day.");
+
+    return 0;
+}
