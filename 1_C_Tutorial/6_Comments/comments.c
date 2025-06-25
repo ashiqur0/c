b9173638c26ea13996ed391de1667e2ba4b49acc
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-25 06:51:42 +0600

    C : Comments

---
+++ 1_C_Tutorial/6_Comments/comments.c
@@ -0,0 +1,20 @@
+/*
+C : Comments
+comments: are use to explain code or prevent execution of alternative code
+
+*/
+
+#include <stdio.h>
+int main() {
+    // single line comments
+
+    // multiline comments:
+    /*
+    printf("Hello World!");        
+    printf("Have a good day.");    
+    */
+
+    printf("Single line Comments");
+
+    return 0;
+}
