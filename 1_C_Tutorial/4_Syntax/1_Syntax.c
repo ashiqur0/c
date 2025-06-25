f1f2c9d2db515872434eeb2fa3325b22c4994ed6
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-25 06:22:57 +0600

    C : Syntax

---
+++ 1_C_Tutorial/4_Syntax/1_Syntax.c
@@ -0,0 +1,16 @@
+/*
+C : Syntax
+
+#include <stdio.h> : standard header that allow to use input-output function in the program. Example. printf() function
+
+main() {}  main function : inside {} all the code will executed.
+return 0 : indicate the ending of the main function
+*/
+
+#include <stdio.h>
+int main() {
+    
+    printf("Hello World!");
+
+    return 0;
+}
