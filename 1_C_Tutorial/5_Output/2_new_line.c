0019194b4ecb5d0f01cd781c3c3e6c11ba09f304
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-25 06:41:27 +0600

    C : New Line

---
+++ 1_C_Tutorial/5_Output/2_new_line.c
@@ -0,0 +1,18 @@
+/*
+C : New Line
+
+\n : use \n (escape-sequence character) to insert a new line.
+
+output:
+Hello World!
+Have a good day.
+*/
+
+#include <stdio.h>
+int main() {
+    
+    printf("Hello World!\n");
+    printf("Have a good day.");
+
+    return 0;
+}
