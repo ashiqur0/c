b9e35c15fd39eade31c7140c7af3cf716c65f56a
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-06-25 06:28:26 +0600

    C : Statement

---
+++ 1_C_Tutorial/4_Syntax/2_Statement.c
@@ -0,0 +1,14 @@
+/*
+C : Statement
+
+printf("Hello World!"); : is a statements
+';' : semicolon is statement terminator
+*/
+
+#include <stdio.h>
+int main() {
+    
+    printf("Hello World!");
+
+    return 0;
+}
