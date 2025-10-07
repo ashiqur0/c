1311d5b601ab682c3f84062776b5fd367f5f8dc3
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-10-07 10:43:17 +0600

    Even Odd and Negative Positive Number Detection

---
+++ Introduction_To_Programming/Module_2/Extra_Practice_Problem/even_odd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+
+    int num;
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("%d is a even number\n", num);
+    } else {
+        printf("%d is a odd number\n", num);
+    }
+
+    return 0;
+}
